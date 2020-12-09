2821331d6caef042e13dadfc56db41f5c533e898
quynhbuitruc <quynhbui270492@gmail.com>
2020-12-09 23:26:28 +0900

    create new bunny

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-let ctx = document.getElementById("game").getContext("2d")
-let strength = document.getElementById("strength")
-let growth = document.getElementById("growth")
+let bunny = new Rabits ()
+bunny.draw()
