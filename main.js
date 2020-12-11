be5795e1deb5979bac1d8b9db8594450182a0324
quynhbuitruc <quynhbui270492@gmail.com>
2020-12-11 22:16:44 +0900

    creat onkeypress event

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ let canvas = document.getElementById("game")
 let game = new Game(canvas)
 window.requestAnimationFrame(tick)
 
+window.onkeypress = function (event) {
+    game.controlBunny(event.keyCode)
+}
 
 function tick() {
-  game.draw()
-  window.requestAnimationFrame(tick)
+    game.drawBunny()
+    window.requestAnimationFrame(tick)
 }
+
