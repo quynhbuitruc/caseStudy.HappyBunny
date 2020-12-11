dc820608e48e8946d02980760b6de97b6c369537
quynhbuitruc <quynhbui270492@gmail.com>
2020-12-11 22:45:23 +0900

    put img out of function, set conditions for this.x

--- bunny.js
+++ bunny.js
@@ -1,14 +1,17 @@
 function Rabits() {
     this.x = 250
     this.y = 250
+    let img = new Image()
+    img.src = "bunny.png"
     this.draw = function (ctx) {
-        let img = new Image()
-        img.src = "bunny.png"
         ctx.drawImage(img, this.x, this.y)
     }
     this.moveRight = function (ctx) {
         ctx.clearRect(this.x, this.y, 50, 50)
-        this.x +=50
+        if (this.x + 50 < 550) {
+            this.x += 10
+        }
         ctx.drawImage(img, this.x, this.y)
     }
+    
 }
