2de713284fa6a2fe3a88262062eabe0d4597bc38
quynhbuitruc <quynhbui270492@gmail.com>
2020-12-11 23:37:23 +0900

    add function eraseCarrot

--- carrots.js
+++ carrots.js
@@ -3,7 +3,12 @@ function Carrots() {
     this.y = Math.round(Math.random() * 250)
     let img = new Image()
     img.src = "carrots.png"
-    this.draw = function (ctx) {
-        ctx.drawImage(img, this.x, this.y)
+    this.draw = function (ctx, bunny) {
+        if (bunny.x !== this.x || bunny.y !== this.y) {
+            ctx.drawImage(img, this.x, this.y)
+        }
+    }
+    this.eraseCarrot = function (ctx) {
+        ctx.clearRect(this.x, this.y, 30, 30)
     }
 }
