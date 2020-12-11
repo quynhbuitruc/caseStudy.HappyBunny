f8c028c904379dbb0a7255c009d22987452a4c09
quynhbuitruc <quynhbui270492@gmail.com>
2020-12-11 22:35:23 +0900

    let object bunny out, creat function

--- Game.js
+++ Game.js
@@ -2,9 +2,17 @@ function Game(canvas) {
   this.canvas = canvas
 
   let ctx = this.canvas.getContext("2d");
+  let bunny = new Rabits()
 
-  this.draw = function () {
-    let bunny = new Rabits()
+  this.drawBunny = function () {
     bunny.draw(ctx)
+  }
+
+  this.controlBunnyMove = function (keyCode) {
+    switch (keyCode) {
+      case 39:
+        bunny.moveRight(ctx)
+        break
+    }
   }
 }
