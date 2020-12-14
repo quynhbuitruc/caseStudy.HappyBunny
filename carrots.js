5e8913bff3ca78128d1d8263ff6954f2304068a1
quynhbuitruc <quynhbui270492@gmail.com>
2020-12-14 23:35:45 +0900

    delete condition in function draw (classCarrots)

--- carrots.js
+++ carrots.js
@@ -1,13 +1,11 @@
 function Carrots() {
-    this.x = Math.round(Math.random() * 550)
-    this.y = Math.round(Math.random() * 550)
-
+    this.x = Math.round(Math.random() * 520)
+    this.y = Math.round(Math.random() * 520)
+    
     let img = new Image()
     img.src = "carrots.png"
 
-    this.draw = function (ctx, bunny) {
-        if (bunny.x !== this.x || bunny.y !== this.y) {
-            ctx.drawImage(img, this.x, this.y)
-        }
+    this.draw = function (ctx) {
+        ctx.drawImage(img, this.x, this.y)
     }
 }
