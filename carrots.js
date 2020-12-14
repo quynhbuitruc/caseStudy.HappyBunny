513877e187fd466c75c6de12886e9ae1bfc22f52
quynhbuitruc <quynhbui270492@gmail.com>
2020-12-14 22:22:55 +0900

    reset this.x and this.y trong class Carrots

--- carrots.js
+++ carrots.js
@@ -1,14 +1,13 @@
 function Carrots() {
-    this.x = Math.round(Math.random() * 250)
-    this.y = Math.round(Math.random() * 250)
+    this.x = Math.round(Math.random() * 550)
+    this.y = Math.round(Math.random() * 550)
+
     let img = new Image()
     img.src = "carrots.png"
+
     this.draw = function (ctx, bunny) {
         if (bunny.x !== this.x || bunny.y !== this.y) {
             ctx.drawImage(img, this.x, this.y)
         }
-    }
-    this.eraseCarrot = function (ctx) {
-        ctx.clearRect(this.x, this.y, 30, 30)
     }
 }
