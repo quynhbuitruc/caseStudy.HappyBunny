c5a8406109d908893ba88dce684f458f84ccb16b
quynhbuitruc <quynhbui270492@gmail.com>
2020-12-14 23:49:51 +0900

    remove clearRect in Rabits class

--- bunny.js
+++ bunny.js
@@ -6,29 +6,33 @@ function Rabits() {
     this.draw = function (ctx) {
         ctx.drawImage(img, this.x, this.y)
     }
+
+    this.getLeft = function () {
+        return this.x
+    }
+    this.getTop = function () {
+        return this.y
+    }
+
     this.moveRight = function (ctx) {
-        ctx.clearRect(this.x, this.y, 50, 50)
         if (this.x + 50 < 550) {
             this.x += 10
         }
         ctx.drawImage(img, this.x, this.y)
     }
     this.moveLeft = function (ctx) {
-        ctx.clearRect(this.x, this.y, 50, 50)
         if (this.x > 10) {
             this.x -= 10
         }
         ctx.drawImage(img, this.x, this.y)
     }
     this.moveUp = function (ctx) {
-        ctx.clearRect(this.x, this.y, 50, 50)
         if (this.y > 10) {
             this.y -= 10
         }
         ctx.drawImage(img, this.x, this.y)
     }
     this.moveDown = function (ctx) {
-        ctx.clearRect(this.x, this.y, 50, 50)
         if (this.y + 50 < 550) {
             this.y += 10
         }
