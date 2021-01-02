9b1cd83ef16992d9b4ae1f70f21850a313a12863
quynhbuitruc <quynhbui270492@gmail.com>
2021-01-02 21:49:05 +0900

    edit positions of carrots

--- carrots.js
+++ carrots.js
@@ -1,6 +1,6 @@
 function Carrots(ctx) {
-    this.x = Math.round(Math.random() * (ctx.canvas.width - 25))
-    this.y = Math.round(Math.random() * (ctx.canvas.height - 25))
+    this.x = Math.round(Math.random() * (ctx.canvas.width - 50))
+    this.y = Math.round(Math.random() * (ctx.canvas.height - 50))
     
     let img = new Image()
     img.src = "carrots.png"
