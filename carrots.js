8e0a599e84c99bffcad193d54235ff1a79b7c80d
quynhbuitruc <quynhbui270492@gmail.com>
2021-01-31 00:04:16 +0900

    change pos of carrots

--- carrots.js
+++ carrots.js
@@ -1,6 +1,6 @@
 function Carrots(ctx) {
-    this.x = Math.round(Math.random() * (ctx.canvas.width - 50))
-    this.y = Math.round(Math.random() * (ctx.canvas.height - 50))
+    this.x = Math.round(Math.random() * (ctx.canvas.width - 100))
+    this.y = Math.round(Math.random() * (ctx.canvas.height - 100))
     
     let img = new Image()
     img.src = "carrots.png"
